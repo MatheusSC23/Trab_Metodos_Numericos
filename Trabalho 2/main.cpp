8860d4b36d617bb3e06cf32c7b18da07e91ab396
Tibet Teixeira <tibetbrasil@gmail.com>
2018-11-24 22:18:04 -0300

    Sistema calibrado

--- Trabalho 2/main.cpp
+++ Trabalho 2/main.cpp
@@ -8,14 +8,15 @@ using namespace std;
 int main() {
     int n = 3;
 
-    double vec[n] = {16,38,38};
+    double vec[n] = {8,6,11};
     double** mat = (double**) malloc(n * sizeof(double*));
     for(int i = 0; i < n; i++){
         mat[i] = (double*) malloc(n * sizeof(double));
     }
-    mat[0][0] = 20; mat[0][1] = 7; mat[0][2] = 9;
-    mat[1][0] = 7; mat[1][1] = 30; mat[1][2] = 8;
-    mat[2][0] = 9; mat[2][1] = 8; mat[2][2] = 30;
+    
+    mat[0][0] = 3; mat[0][1] = -2; mat[0][2] = 1;
+    mat[1][0] = 1; mat[1][1] = -3; mat[1][2] = 4;
+    mat[2][0] = 9; mat[2][1] = 4; mat[2][2] = -5;
 
     Mat matrix(mat, n);
     Vec vector(vec, n);
